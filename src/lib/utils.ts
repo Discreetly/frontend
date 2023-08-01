391e266d8eb78dcddefe6f832fc1a50702622096
AtHeartEngineer <atheartengineer@gmail.com>
2023-07-31 21:11:45 -0400

    chore refactored "updateServer"

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,11 @@
-import { randomBigInt, genId } from 'discreetly-interfaces';
-import type { ServerI, ServerListI } from 'discreetly-interfaces';
+import type { ServerI } from 'discreetly-interfaces';
 import { serverDataStore, serverListStore } from './stores';
 import { get } from 'svelte/store';
+
+interface ServerListI {
+	name: string;
+	url: string;
+}
 
 async function fetchServer(server_url: string): Promise<ServerI | void> {
 	console.debug(`Fetching server ${server_url}`);
@@ -20,28 +24,43 @@ async function fetchServer(server_url: string): Promise<ServerI | void> {
 		});
 }
 
-function updateServers(): { [key: string]: ServerI } {
-	if (get(serverListStore).length < 1) {
+export async function updateServers(): Promise<{ [key: string]: ServerI }> {
+	const serverList = get(serverListStore);
+
+	if (serverList.length < 1) {
 		console.error('serverListStore is empty');
 		serverListStore.set([{ name: 'Localhost', url: 'http://localhost:3001/api/' } as ServerListI]);
 	}
-	get(serverListStore).forEach((server: ServerListI) => {
-		console.log('fetching server data');
-		fetchServer(server.url).then((data) => {
-			console.log('setting server data');
-			const temp = get(serverDataStore);
-			if (temp[server.url]) {
-				Object.assign(temp[server.url], data as ServerI);
-			} else {
-				temp[server.url] = data as ServerI;
-			}
-			temp[server.url].selectedRoom = temp[server.url].rooms[0]
-				? temp[server.url].rooms[0].roomId
-				: '';
-			serverDataStore.set(temp);
-		});
-	});
-	return serverDataStore;
-}
 
-export { randomBigInt, genId, fetchServer, updateServers };
+	const newServerData: { [key: string]: ServerI } = {};
+	const oldServerData = get(serverDataStore);
+
+	await Promise.all(
+		serverList.map(async (server: ServerListI) => {
+			console.log(`Fetching server data from ${server.url}`);
+			const data = await fetchServer(server.url);
+			console.log(`Setting server data for ${server.url}`);
+			if (data) {
+				newServerData[server.url] = { ...data };
+				newServerData[server.url].rooms = newServerData[server.url].rooms || [];
+
+				if (oldServerData[server.url]?.selectedRoom !== undefined) {
+					newServerData[server.url].selectedRoom = oldServerData[server.url].selectedRoom;
+				} else if (newServerData[server.url].rooms?.length) {
+					newServerData[server.url].selectedRoom = String(
+						newServerData[server.url].rooms[0].roomId
+					);
+				} else {
+					newServerData[server.url].selectedRoom = undefined;
+				}
+			}
+		})
+	);
+
+	serverDataStore.update((store: { [key: string]: ServerI } = {}) => ({
+		...store,
+		...newServerData
+	}));
+
+	return newServerData;
+}
