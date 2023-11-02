53ff6c93bcf1958a25768d43f5f1cf54f5e7db98
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2023-11-02 06:27:12 +0300

    welcome page header cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,8 @@
 	);
 </script>
 
-<div class="mx-5 lg:mx-auto mt-16 max-w-[80ch]">
-	<h1 class="h1 mb-10">Welcome to Discreetly!</h1>
+<div class="mx-5 lg:mx-auto mt-8 md:mt-16 max-w-[80ch]">
+	<h1 class="h1 mb-6 md:mb-10 text-center">Welcome to Discreetly!</h1>
 	<div class="mb-5">
 		<Introduction />
 	</div>
